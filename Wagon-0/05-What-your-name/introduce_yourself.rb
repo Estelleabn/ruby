a6ee8bd4766e26f6ead4bca6ac120b7391008a76
Estelleabn <estelleaubouin@gmail.com>
2014-01-06 17:29:43 +0100

    solution 1 pour ruby-0-ex5

--- Wagon-0/05-What-your-name/introduce_yourself.rb
+++ Wagon-0/05-What-your-name/introduce_yourself.rb
@@ -1,1 +1,21 @@
 # your code goes here, introduce yourself !
+
+puts "Quel est ton prenom ?"
+first_name = gets.chomp
+puts "Un deuxieme prenom peut-etre ?"
+middle_name = gets.chomp
+puts "Quel est ton nom de famille ?"
+last_name = gets.chomp
+
+
+length = (first_name + middle_name + last_name).length
+
+puts "Hello" + " " + first_name + " " + middle_name + " " + last_name + " !"
+puts "Hello #{first_name} #{middle_name} #{last_name} !"
+
+puts "Ton nom fait #{length} lettres, CHOUETTOS"
+
+if first_name == "Estelle" && middle_name == "A" && last_name == "Abn" 
+		puts "You're a good person"
+	end
+
