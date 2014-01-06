ea557b8eac7f3ffaff3aaa66f8381212b40f4473
Estelleabn <estelleaubouin@gmail.com>
2014-01-06 16:56:34 +0100

    solution 1 pour ruby-0-ex4

--- Wagon-0/04-Age-in-days/age_in_days.rb
+++ Wagon-0/04-Age-in-days/age_in_days.rb
@@ -1,7 +1,13 @@
 def age_in_days(day, month, year)
-  # your code goes here !
+  t = Time.now.to_i
+  t2 = Time.new(year, month, day).to_i
+  t3 = (t - t2) / (60*60*24)
+  puts "Tu as #{t3} jours"
+
+
 end
 
 
 # Testing your code
 puts age_in_days(6, 11, 1985)
+
