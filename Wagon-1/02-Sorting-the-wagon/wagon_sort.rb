83ad2c4aaeb5b9473a29aa554b36957eea6f03e2
Estelleabn <estelleaubouin@gmail.com>
2014-01-07 16:55:38 +0100

    solution exo ruby1-02

--- Wagon-1/02-Sorting-the-wagon/wagon_sort.rb
+++ Wagon-1/02-Sorting-the-wagon/wagon_sort.rb
@@ -1,5 +1,29 @@
-def wagon_sort( students_array )
-  # your code to sort the array and output students
+def wagon_sort(students_array)
+
+  students_array.sort!
+  
+  for name in students_array
+  	puts name
+  end
 end
+
+puts "Type a student name"
+name1 = gets.chomp
+array = [name1]
+
+puts "Type another student name"
+name2 = gets.chomp
+array << name2
+
+puts "Type a third name"
+name3 = gets.chomp
+array << name3
+
+puts "Type a fourth name"
+name4 = gets.chomp
+array << name4
+
+
+wagon_sort(array)
 
 # your code to interact with the user..
