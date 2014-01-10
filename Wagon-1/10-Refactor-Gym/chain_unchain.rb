b92a3c55cf8053db90b4c0c914a8203864dce9db
Estelleabn <estelleaubouin@gmail.com>
2014-01-10 10:56:11 +0100

    Solution ruby-1-exo10 YEAH

--- Wagon-1/10-Refactor-Gym/chain_unchain.rb
+++ Wagon-1/10-Refactor-Gym/chain_unchain.rb
@@ -1,20 +1,30 @@
 def shuffle_word(a_word)
   # Ruby fonction to play to "Des chiffres et des lettres.."
   
-  cap_word = a_word.upcase
-  chars_enum = cap_word.chars
-  array = chars_enum.to_a
-  shuffle_array = array.shuffle
-  
-  return shuffle_array
+  a_word.upcase.chars.to_a.shuffle
   
 end
 
+# Version originale
 
 def quote_prime_numbers(n)
   (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
 end
 
+# Version un peu plus étendue
+
+def quote_prime_integers(n)
+  prime_integers = (1..n).find_all do |i|
+  	if (2..i-1).select {|k| i % k == 0 }.count == 0
+      true
+	end
+end
+
+  a.map{ |prime_num| "#{prime_num} is prime"}
+
+end
+
 
 puts shuffle_word("nabuchodonosor")
 puts quote_prime_numbers(30)
+puts quote_prime_integers(40)
