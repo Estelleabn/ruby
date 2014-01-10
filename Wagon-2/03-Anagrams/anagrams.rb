fcce74782b7809ecd2f5ea5a5d8a4797f9bbb191
Estelleabn <estelleaubouin@gmail.com>
2014-01-10 17:44:10 +0100

    Solution ruby Wagon-2-exo03

--- Wagon-2/03-Anagrams/anagrams.rb
+++ Wagon-2/03-Anagrams/anagrams.rb
@@ -1,9 +1,22 @@
 def anagrams?( a_string, another_string )
-  # your code goes here
+ 
+ # Attention aux espaces
+ 
+ a_string.delete(" ").downcase.chars.to_a.sort ==  another_string.delete(" ").downcase.chars.to_a.sort
+  
 end
 
 def anagrams_on_steroids?( a_string, another_string )
-  # your code goes here
+ 
+    hash1 = Hash.new{|h, k| h[k] = 0}
+    hash2 = Hash.new{|h, k| h[k] = 0}
+
+  a_string.delete(" ").downcase.split(//).each {|x| hash1[x]+=1}
+  another_string.delete(" ").downcase.split(//).each {|x| hash2[x]+=1}
+
+hash1 == hash2
+
+   
 end
 
 # complexity = ?
