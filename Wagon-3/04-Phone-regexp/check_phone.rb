15ffe9e49218d2b484e1e8da1179728e790d7e62
Estelleabn <estelleaubouin@gmail.com>
2014-01-14 17:18:22 +0100

    solution Wagon-3 exo 4 sur les numéros de tel

--- Wagon-3/04-Phone-regexp/check_phone.rb
+++ Wagon-3/04-Phone-regexp/check_phone.rb
@@ -1,24 +1,14 @@
 # Check your 06
 
-# Writes a program that checks if a phone number is a valid french number.
-# A french phone number should contain 10 numbers and starting with 0 or 12 numbers starting with +33
-# Also you can accept phone number with delimiters between numbers.
-
-# Objective
-# How to use regex
-
-
-# Help
-# http://rubular.com/
-
-
 def is_valid_phone_number(phone_number)
-  # your code here
+
+	if phone_number.match(/(0|\+33)( |-||)[1-7](( |-||)\d{2}){4}/)
+		true
+	else
+		false
+	end
+
 end
-
-
-
-# tests
 
 puts is_valid_phone_number("0665363636") # should output true
 puts is_valid_phone_number("06 65 36 36 36") # should output true
